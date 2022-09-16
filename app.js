0eeb6454b85bb86947f11601c2024b34f47dc61b
Garvit Yadav <48787184+garvityadav@users.noreply.github.com>
2022-09-16 21:08:56 +0530

    third commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require('express-async-errors');
 const helmet =require('helmet');
 const cors =require('cors');
 const xss = require('xss-clean');
-const rateLimiter = require('express-rate-limit');
+
 
 //express
 const express = require('express');
@@ -27,11 +27,7 @@ const connectDB = require('./db/connect');
 
 app.use(express.json());
 // extra packages
-app.set('trust proxy', 1)
-app.use(rateLimiter({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, 
-}));
+
 app.use(helmet());
 app.use(cors());
 app.use(xss());
